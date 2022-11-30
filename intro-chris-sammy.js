ec7846e79adb644f3cf81a3efa5caf2f8a1b9182
SammyT13 <sammyt1316@yahoo.com>
2022-11-30 12:00:31 -0800

    updated with partially completed of the last part of the challenges

--- intro-chris-sammy.js
+++ intro-chris-sammy.js
@@ -78,3 +78,24 @@ console.log("LEARN"[0] === "l")
 // Modify the code from the previous question to return true.
 
 console.log("LEARN"[0] === "L")
+
+// Consider the variables:
+
+var theQuestion = 'life, the universe, and everything'
+var theAnswer = 42
+
+// Write the code that will log theAnswer divided by 2.
+
+console.log(theAnswer / 2)
+
+// Write the code that will log the length of theQuestion.
+
+console.log(theQuestion.length)
+
+// Write the code that will log the index of the character "f" in the theQuestion.
+
+console.log(theQuestion.indexOf('f'))
+// Write the code that will log the concatenation of the two variables.
+// Write the code that will log "the universe".
+// Write the code that will log the character "l" from theQuestion.
+// Write the code that will log whether theQuestion.length is greater than theAnswer.
