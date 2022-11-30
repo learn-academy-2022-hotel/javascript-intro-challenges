88a3642146467a960479b4d91e98e29356265aa2
djx-ctrl <cras.dtravis@gmail.com>
2022-11-30 12:42:40 -0800

    added js challenges

---
+++ intro-dominque-dave.js
@@ -0,0 +1,13 @@
+console.log(34+71)
+console.log(123-67)
+console.log(56*23)
+console.log(45/5)
+console.log(5**7)
+console.log(33%6)
+console.log("Dom".length)
+let greeting = "Dom"
+console.log(greeting.includes("e"))
+let tag = "Dave"
+console.log(tag.includes("v"))
+console.log("hello"[0])
+console.log(tag.toUpperCase())
