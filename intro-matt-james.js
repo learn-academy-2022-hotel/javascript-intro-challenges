e5e57f5a4a5596cbd537a925184d331615730f60
James Quillen <james.quillen@gmail.com>
2022-11-30 12:00:16 -0800

    Completed half of the second third of questions before lunch break

--- intro-matt-james.js
+++ intro-matt-james.js
@@ -19,3 +19,20 @@ console.log(name.includes("e"));
 console.log(name[0]);
 // Write the code that will log the string in all uppercase letters.
 console.log(name.toUpperCase());
+
+
+
+// Is 34 divided by 3 greater than 67 divided by 2?
+console.log((34 / 3) > (67 / 2));
+// Does 5 evaluate to the same as "5"?
+console.log(5 == "5");
+// Does 5 strictly equal "5"?
+console.log(5 === "5");
+// Does !3 strictly equal 3?
+console.log(!3 === 3);
+// Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+// Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+// Does "LEARN" contain the subset "RN"?
+// Does "LEARN" contain the subset "rn"?
+// Does "LEARN"[0] strictly equal "l"?
+// Modify the code from the previous question to return true.
