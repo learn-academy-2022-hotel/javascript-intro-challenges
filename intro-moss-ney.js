2b69c022d34aeb0bf198c9c906f353c84fe3b1e5
mosscantrell <mossc2001@gmail.com>
2022-11-30 14:45:09 -0800

    began the javascript introduction challenges

---
+++ intro-moss-ney.js
@@ -0,0 +1,22 @@
+console.log(34 + 71)
+console.log(67 - 123)
+console.log(56 * 23)
+console.log(45 / 5)
+console.log(5 ** 7)
+console.log(33 % 6)
+
+let myName = "moss"
+console.log(myName.length)
+console.log(myName.includes("e"))
+console.log(myName[0])
+console.log((34 / 3) > (67 / 2))
+console.log(5 * 5)
+console.log(5 === 5)
+console.log(eval(5))
+console.log(!3 === 3)
+
+let message = "learn"
+console.log(message.length === 5)
+console.log("student".length === 7, "learn".length === 7)
+console.log("LEARN".includes("rn"))
+console.log("LEARN"[0] === )
