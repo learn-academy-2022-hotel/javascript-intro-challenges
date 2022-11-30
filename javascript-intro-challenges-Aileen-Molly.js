b4bdb040fd7832e84815ef67e8b2f92253850c8c
mollykwolf <mollykwolf@gmail.com>
2022-11-30 15:07:09 -0800

    Javascript Challenge 1

---
+++ javascript-intro-challenges-Aileen-Molly.js
@@ -0,0 +1,23 @@
+//Write the code that will log the outcome of 34 added to 71.
+console.log(34+71)
+//Write the code that will log the outcome of 67 subtracted from 123.
+console.log(123-67)
+//Write the code that will log the outcome of 56 multiplied by 23.
+console.log(56*23)
+//console.warn();rite the code that will log the outcome of 45 divided by 5.
+console.log(45/5)
+//Write the code that will log the outcome of 5 to the power of 7.
+console.log(5**7)
+//Write the code that will log the whole number remainder of 33 divided by 6.
+console.log(33%6)
+//Write the code that will log the length of a string containing your name.
+let message = "Molly"
+//Write the code that will log the length of a string containing your name.
+console.log(message.length)
+////Write the code that will log whether your string includes the letter "e"?
+console.log(message.includes("e"))
+//Write the code that will log the character at the first index of the string.
+console.log(message [0])
+//Write the code that will log the string in all uppercase letters.
+let result = message.toUpperCase();
+console.log(result)
