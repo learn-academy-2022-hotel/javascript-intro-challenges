ec4071b28e7cd10aca9c9b2f2a6774748975e31a
mosscantrell <mossc2001@gmail.com>
2022-12-01 13:33:46 -0800

    began array challenges

---
+++ arrays-moss-molly.js
@@ -0,0 +1,47 @@
+// var groceryList = ["chips", "dip", "cookies"]
+
+// Write the code that will add "soda" to the end of the original array.
+
+// console.log(groceryList)
+// groceryList.push("soda")
+// console.log(groceryList)
+
+// Write the code that will add "granola" to the end of array without altering the original array.
+
+// var newItem = ["granola"]
+// console.log(groceryList, newGroceryList)
+// groceryList.concat(newItem)
+// var itemAndList = groceryList.concat(newItem)
+// console.log(itemAndList)
+
+// Write the code that will return a subset of the array containing only "chips" and "dip".
+
+// console.log(groceryList.slice(0, 2))
+// var chipsAndDip = groceryList.slice(0, 2)
+// requires starting and ending index, and index for "cookies" is 2. not sure why 2 does not remove "cookies"
+
+// Write the code that will add "beans" to the "chips" and "dip" array.
+
+// chipsAndDip.push("beans")
+// console.log(chipsAndDip)
+
+
+
+var numSet = [2, 13, 6, 8, 4, 2]
+
+// Write the code that finds the index of the first appearance of the number 2.
+
+// numSet.indexOf(2)
+// var indexOfTwo = numSet.indexOf(2)
+// console.log(indexOfTwo)
+
+// Write the code that finds the index of the last appearance of the number 2.
+
+// numSet.lastIndexOf(2)
+// var indexOfTwo = numSet.lastIndexOf(2)
+// console.log(indexOfTwo)
+
+
+// Write the code that returns the number at the third index.
+
+// console.log(numSet[3])
