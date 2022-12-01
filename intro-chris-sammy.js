2fec843754ad38121f2d1239f78ab916b431a84d
chrisyhlee <leechr8393@gmail.com>
2022-11-30 16:09:02 -0800

    completed the last few challenges for intro challanges

--- intro-chris-sammy.js
+++ intro-chris-sammy.js
@@ -98,6 +98,16 @@ console.log(theQuestion.indexOf('f'))
 
 // Write the code that will log the concatenation of the two variables.
 
+console.log(theQuestion + theAnswer)
+
 // Write the code that will log "the universe".
+
+console.log(theQuestion.slice(6,18))
+
 // Write the code that will log the character "l" from theQuestion.
+
+console.log(theQuestion.charAt(0))
+
 // Write the code that will log whether theQuestion.length is greater than theAnswer.
+
+console.log(theQuestion.length > theAnswer)
