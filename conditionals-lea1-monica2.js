8a5956839b365c641fe624a72de70046bf174a34
monicaarganda <argandamonica1@gmail.com>
2022-11-30 16:51:04 -0800

    added challenge 1-4 answers

---
+++ conditionals-lea1-monica2.js
@@ -0,0 +1,50 @@
+// Conditional Challenges
+
+// Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+
+// let item = 20
+
+// if(item <= 100) {
+//     console.log("in budget")
+// }
+
+// Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+
+// let you = "hungry"
+// let you = "im full"
+
+// if(you === "hungry") {
+//     console.log("eat food")
+// } else {
+//     console.log("keep coding")
+// }
+
+// Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+
+// let trafficLight = "green"
+// trafficLight = "blue"
+// trafficLight = "yellow"
+// trafficLight = "red"
+
+// if(trafficLight === "green") {
+//     console.log("go")
+// } else if(trafficLight === "yellow") {
+//     console.log("slow down")
+// } else if(trafficLight === "red") {
+//     console.log("stop")
+// }
+
+// Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
+
+let x = 6
+let y = 6
+
+if(x > y) {
+    console.log(`${x}`)
+} else if(y > x) {
+    console.log(`${y}`)
+} else if(x === y) {
+    console.log("the numbers are the same")
+}
+
+// Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
