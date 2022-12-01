5a464f891d6b9fc6463ceeac3f36d8440b7750d8
djx-ctrl <cras.dtravis@gmail.com>
2022-11-30 16:33:23 -0800

    conditionalchallenge

--- intro-dominque-dave.js
+++ intro-dominque-dave.js
@@ -1,16 +1,64 @@
-console.log(34+71)
-console.log(123-67)
-console.log(56*23)
-console.log(45/5)
-console.log(5**7)
-console.log(33%6)
-console.log("Dom".length)
-let greeting = "Dom"
-console.log(greeting.includes("e"))
-let tag = "Dave"
-console.log(tag.includes("v"))
-console.log("hello"[0])
-console.log(tag.toUpperCase())
-console.log(34/3 > 67/2)
-console.log(5 === "5")
-console.log(!3 !== 3)
+// console.log(34+71)
+// console.log(123-67)
+// console.log(56*23)
+// console.log(45/5)
+// console.log(5**7)
+// console.log(33%6)
+// console.log("Dom".length)
+// let greeting = "Dom"
+// console.log(greeting.includes("e"))
+// let tag = "Dave"
+// console.log(tag.includes("v"))
+// console.log("hello"[0])
+// console.log(tag.toUpperCase())
+// console.log(34/3 > 67/2)
+// console.log(5 === "5")
+// console.log(!3 !== 3)
+
+// Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+let hotdog = 98
+if(hotdog <= 100){
+  console.log("in budget")
+}
+// Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+let hungry = false
+if(hungry === true){
+    console.log("eat food")
+} else {
+    console.log("keep coding")
+}
+// Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+let lightColor = "green"
+if(lightColor === "red"){
+    console.log("stop")
+} else if(lightColor === "yellow"){
+    console.log("slow down")
+} else{
+    console.log("go")
+}
+// Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
+let numberOne = 2
+let numberTwo = 2
+if(numberOne > numberTwo){
+    console.log(`${numberOne}`)
+} else if(numberOne < numberTwo){
+    console.log(`${numberTwo}`)
+} else if(numberOne = numberTwo){
+    console.log("the numbers are the same")
+}
+// Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
+let myNumber = 0
+if(myNumber === 0){
+    console.log("0")
+} else if(myNumber % 2 == 0){
+    console.log("the number is even")
+} else{
+    console.log("odd")
+}
+// 🏔 Stretch Goals
+// Write a statement that takes a variable of a grade percentage and logs the letter grade for that percentage, if the grade is 100% log "perfect score", if the grade is zero log "no grade available."
+
+// Write a statement that takes a variable of a boolean, number, or string data type and logs the data type of the variable. HINT: Check out the JavaScript typeof operator.
+
+// Create a password checker using a single conditional statement. If a user inputs a password with 12 or more characters AND the password includes !, then log "That is a mighty strong password!" If the user’s password is 8 or more characters OR includes !, then log "That password is strong enough." Log "That is not a valid password." for every other input.
+
