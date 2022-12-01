eedfa5c3c9f73ae855ee9485aacaf018354eb185
Fred-H-McBurnett <fhmtechnology@gmail.com>
2022-11-30 18:00:39 -0600

    final version

--- intro-cs-fm.js
+++ intro-cs-fm.js
@@ -25,27 +25,42 @@ console.log(myName.toUpperCase())
 // Write the code that will log true or false for the following:
 //
 // Is 34 divided by 3 greater than 67 divided by 2?
-console.log(34 / 3 <(67 / 2 ))
+console.log((34 / 3) > (67 / 2 ))
 // Does 5 evaluate to the same as "5"?
-console.log(5 == 5)
+console.log(5 == "5")
 // Does 5 strictly equal "5"?
-console.log("5 == 5")
+console.log(5 === "5")
 // Does !3 strictly equal 3?
+console.log(!3 === 3)
 // Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+console.log("LEARN".length === 5 && "Student".length === 7)
 // Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+console.log("LEARN".length === 5 || "Student".length === 10)
 // Does "LEARN" contain the subset "RN"?
+console.log("LEARN".includes("RN"))
 // Does "LEARN" contain the subset "rn"?
+console.log("LEARN".includes("rn"))
 // Does "LEARN"[0] strictly equal "l"?
+console.log("LEARN".charAt(0) === "l")
 // Modify the code from the previous question to return true.
+console.log("LEARN".charAt(0) === "L")
 // Consider the variables:
 
-// var theQuestion = "life, the universe, and everything"
-// var theAnswer = 42
+var theQuestion = "life, the universe, and everything"
+var theAnswer = 42
 // Write the code that will log theAnswer divided by 2.
+console.log(theAnswer / 2)
 // Write the code that will log the length of theQuestion.
+console.log(theQuestion.length)
 // Write the code that will log the index of the character "f" in the theQuestion.
+console.log(theQuestion.indexOf("f"))
 // Write the code that will log the concatenation of the two variables.
+console.log(theQuestion + theAnswer)
 // Write the code that will log "the universe".
+console.log(theQuestion.slice(6, 18))
 // Write the code that will log the character "l" from theQuestion.
+console.log(theQuestion.charAt("0"))
+console.log(theQuestion.indexOf("l"))
 // Write the code that will log whether theQuestion.length is greater than theAnswer.
-
+console.log(theQuestion.length > theAnswer)
+console.log(theQuestion.length < theAnswer)
