1bf737f31ec11edea5d971705ad9339ad90ebf19
priceymineral <tudaeva@gmail.com>
2022-11-30 16:40:47 -0800

    started working on conditionals challenges

---
+++ conditionals-os-rk.js
@@ -0,0 +1,35 @@
+// Make sure you try different options and change the variables to ensure properly working code.
+
+// Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+var budget = 100
+if (budget <= 100) {
+    console.log("in budget")
+}
+
+// Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+let hungry = false
+if (hungry === true) {
+    console.log("eat food") 
+} else {
+    console.log("keep coding!")
+}
+
+// Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+let trafficLight = true
+if (trafficLight === "green") {
+    console.log("go!!!")
+} else if (trafficLight === "yellow") {
+    console.log("slow down!!!")
+} else if (trafficLight === "red") {
+    console.log("stop!!!")
+} else {
+    console.log("???!")
+}
+
+// Write a statement that takes two variables that are numbers and outputs the larger number. 
+// If the numbers are equal, output "the numbers are the same".
+
+// Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
+
+
+
