dc8da61e817e754d4d6f89f7624f84f8cbea5ea2
msawhitney <msawhit3@gmail.com>
2022-11-30 17:06:21 -0800

    finished conditionals challenge

--- javascript-intro-challenges-Aileen-Molly.js
+++ javascript-intro-challenges-Aileen-Molly.js
@@ -21,3 +21,53 @@ console.log(message [0])
 //Write the code that will log the string in all uppercase letters.
 let result = message.toUpperCase();
 console.log(result)
+
+// Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+let item = 90
+if (item <=100 ) {
+  console.log ("in budget")
+}
+
+// Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+// let hungry = false
+let hungry = true
+if (hungry === true) {
+  console.log ("eat food")
+  } else {
+  console.log ("keep coding")
+}
+
+// Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+// let trafficLight = "green"
+let trafficLight = "yellow"
+if (trafficLight === "green") {
+  console.log("go")
+} else if (trafficLight === "yellow") {
+  console.log("slow down")
+} else if (trafficLight === "red") {
+  console.log("stop")
+}
+
+
+// Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
+let number1 = 2
+let number2 = 10
+if (number1 === number2) {
+  console.log ("The numbers are the same.")
+} else if (number1 > number2) {
+  console.log (number1)
+} else if (number2 > number1) {
+  console.log (number2)
+}
+
+// Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
+//let number = 99
+// let number = 98
+let number = 0
+if (number === 0) {
+  console.log ("zero")
+} else if (number % 2==0) {
+  console.log ("even")
+} else {
+  console.log ("odd")
+}
