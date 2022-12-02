4570faece5e2f320011ee679ba73a9b3cf3af398
Rjkingg <kingraq1007@gmail.com>
2022-12-01 16:38:58 -0800

    raq and dom pratice

---
+++ loops-dom-raq.js
@@ -0,0 +1,18 @@
+// Create a for loop that logs each number from 1 - 20.
+const numsArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+for(let i=0; i<21; i++){
+    console.log(i)
+}
+
+// Create a for loop that logs every other number from 1 - 20.
+
+// Create a for loop that logs the result of each number from 1 - 20 tripled.
+
+// Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD".
+
+// Expected output: ODD, 2, ODD, 4, ODD, 6 ...etc
+
+
+
+
+// const num array 
