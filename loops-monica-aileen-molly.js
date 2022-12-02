41d6d5b68b6e75ae1e9c48f7c04401961ae5e21f
monicaarganda <argandamonica1@gmail.com>
2022-12-01 16:33:37 -0800

    added answers for 1 out 4 of the loop challenges

--- loops-monica-aileen-molly.js
+++ loops-monica-aileen-molly.js
@@ -1,8 +1,29 @@
 // Create a for loop that logs each number from 1 - 20.
+const numsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+// for(let i=0; i<20; i++) {
+//     console.log("value:", numsArray[i])
+// }
+
 // Create a for loop that logs every other number from 1 - 20.
+// for(let i=0; i<numsArray.length; i+=2) {
+//     console.log(numsArray[i])
+// }
+
 // Create a for loop that logs the result of each number from 1 - 20 tripled.
+// for(let i=0; i<numsArray.length; i++) {
+//    console.log(numsArray[i] * 3)
+// }
+
 // Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD".
 // Expected output: ODD, 2, ODD, 4, ODD, 6 ...etc
+for(let i=0; i<numsArray.length; i++) {
+    if(numsArray[i] % 2 === 0) {
+        console.log(numsArray[i])
+    } else if(numsArray[i] % 2 > 0) {
+        console.log(`ODD`) 
+    } 
+}
 // Consider this variable:
 
 // const nums = [3, 57, -9, 20, 67]
